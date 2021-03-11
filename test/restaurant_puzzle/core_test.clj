a792a305c43da1885a75447b0b8ff9cd89e2519c
Chad <theChad@users.noreply.github.com>
2021-03-10 18:42:39 -0600

    init

---
+++ test/restaurant_puzzle/core_test.clj
@@ -0,0 +1,7 @@
+(ns restaurant-puzzle.core-test
+  (:require [clojure.test :refer :all]
+            [restaurant-puzzle.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
