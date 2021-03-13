4bc262b104e4d46293a8bdcf2e35a4e66782f036
Chad <theChad@users.noreply.github.com>
2021-03-12 19:56:16 -0600

    Added a couple cartesian product functions.

--- src/restaurant_puzzle/core.clj
+++ src/restaurant_puzzle/core.clj
@@ -11,23 +11,22 @@
   ;; Get dictionary and reverse dictionary.
   (let [phoneme-dictionary (pd/get-phoneme-dictionary "resources/cmudict-0.7b")
         reverse-phoneme-dictionary (pd/get-reverse-phoneme-dictionary)]
-    (println " Top. ")
+    ;; Just look at the top line of clue data.
     (->> (utils/read-clue-words) ; clue word-lists
          (utils/words-to-phonemes phoneme-dictionary) ; clue phoneme lists
-         first
-         (apply combo/cartesian-product)
-         (map combo/permutations)
+         first ; first line only
+         (apply combo/cartesian-product) ; all pronunciations per word
+         (map combo/permutations) ; all combinations of words
          (reduce into)
          (map flatten)
+         ;; Get possible words from the phoneme lists
          (map (partial pc/get-words-from-phonemes reverse-phoneme-dictionary))
          (reduce into)
-         (remove (partial utils/uses-all-base-words? (utils/read-clue-words)))
-         ;;(#(do (println "remove. " %) %))
+         ;; Remove entries with that still use almost all of the base words
+         (remove (partial utils/uses-most-base-words? 3 (utils/read-clue-words)))
          (pc/string-from-word-chains)
          (spit "resources/answers.txt")
          )))
-(-main)
 
-;;(combo/permutations [1 2 3])
 
 
--- src/restaurant_puzzle/utils.clj
+++ src/restaurant_puzzle/utils.clj
@@ -38,6 +38,15 @@
   (empty? (clojure.set/difference (set (flatten base-words)) (set (flatten word-chain))))
   )
 
+(defn uses-most-base-words?
+  "True if a word chain lacks at most n of the base words.
+   word-chain: [[homonymns] [homonymns2] ..]
+   base-words: [w1 w1..]"
+  [n base-words word-chain]
+  ;; Technically, this won't count homonmyms in the base pair as separate
+  ;; words, but that's probably fine here.
+  (>= n (count (clojure.set/difference (set (flatten base-words)) (set (flatten word-chain)))))
+  )
 
 
 
@@ -46,12 +55,17 @@
                           (read-clue-words "resources/clue_words.txt")))
 
 
-(remove (partial uses-all-base-words? [[["this" "is" "a" "test"]]])                      
-        [[["this" "and"] ["a"] ["is"] "tess"]])
+(remove (partial uses-most-base-words? 1 [[["this" "is" "a" "test"]]])                      
+        [[["this" "and"] ["a"] ["is"] "tes"]])
+(uses-most-base-words? 1 [[["this" "is" "a" "test"]]]                      
+                       [[["this" "and"] ["a"] ["is"] "tes"]])
+
 
 
 ;;; This is just clojure.math.combinatorics/cartesian-product.
 ;;; I'd still like to come back to a recurseive version sometime.
+
+;;; pronunciation-combos doesn't work.
 
 (defn pronunciation-combos
   "Return all combinations of pronunciations of words from a collection
@@ -78,3 +92,38 @@
   ([p-poss]
    (pronunciation-combos p-poss [] [])))
 
+;;; Working, non-memoized cartesian product. No tail recursion.
+;;; Does use an accumulator though.
+
+(defn cartesian-product
+  "Recursive cartesian product of vectors in the factors sequence."
+  ([factors working-product]
+   (cond (empty? factors) [working-product]
+         ;; Scalar value? Just append it and move onto the rest of the factors
+         (not (coll? (first factors))) (cartesian-product (rest factors) (conj working-product (first factors)))
+         ;; Ignore empty vectors. Could also see letting it return an empty vector for final product
+         (empty? (first factors)) (cartesian-product (rest factors) working-product)
+         ;; Continue the working product with each of the availble values in the first factor.
+         :else (reduce into (map #(cartesian-product (rest factors) (conj working-product %)) (first factors)))))
+  ([factors]
+   (cartesian-product factors [])))
+
+;;; Memoized cartesian product, but also without tail recursion.
+
+(def cartesian-product-mem
+  "Recursive cartesian product of vectors in the factors sequence."
+  (memoize (fn [factors]
+             (println "ran it")
+             (cond (empty? factors) ['()]
+                   ;; Scalar value? Just append it and move onto the rest of the factors
+                   ;;(not (coll? (first factors))) (cartesian-product (rest factors) (conj working-product (first factors)))
+                   ;; Ignore empty vectors. Could also see letting it return an empty vector for final product
+                   ;;(empty? (first factors)) (cartesian-product (rest factors) working-product)
+
+                   ;; Continue the working product with each of the availble values in the first factor.
+                   :else (for [first-element (first factors)
+                               cart-prod (cartesian-product-mem (rest factors))]
+                           (conj cart-prod first-element))))))
+
+
+(cartesian-product-mem [[1 2 10] [4 5 7] [7 8 9]])
